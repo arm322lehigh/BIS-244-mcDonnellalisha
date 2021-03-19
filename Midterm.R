cdc7a146ed2150670b00cdd5cb8b2177b11ec225
Alisha McDonnell <arm322@lehigh.edu>
2021-03-19 11:47:24 -0700

    Midterm submitted

---
+++ Midterm.R
@@ -0,0 +1,86 @@
+#Alisha McDonnell Midterm R.
+
+
+# Clear console
+rm(list=ls(all=TRUE))
+cat("\014")
+
+# Load packages
+library(gapminder)
+library(here)
+library(tidyverse)
+
+# Reading in file
+
+midterm.r <- read.csv("AAPL.csv")
+
+View(midterm.r)
+
+n <- length(midterm.r$Adj.Close)
+
+
+# Initialize new variable in data frame
+midterm.r$incr_cases <- 1
+
+View(midterm.r)
+
+# Calculate values for other than first row using FOR loop
+for (i in 2:n) {
+  midterm.r$incr_cases[i] <- (midterm.r$Adj.Close[i]-midterm.r$Adj.Close[i-1]) 
+}
+
+# Plotting
+p <- ggplot(data = midterm.r,
+            mapping = aes(x = Date,
+                          y = incr_cases))
+p + geom_point() +
+  labs(x = "03/19/2020-03/18/2021", y = "Change in Adjusted Closing Price",
+       title = "Changes in AAPL Daily Prices over Last Year",
+       subtitle = "Alisha McDonnell",
+       caption = "Exam 1")
+
+#finding the mean
+mean(midterm.r$incr_cases)
+
+mean_adj <-mean(midterm.r$incr_cases)
+
+for (i in 1:n) {
+  if(midterm.r$incr_cases[i]>=mean_adj) {
+    midterm.r$above_cases[i] <- midterm.r$incr_cases[i]
+  } else {
+    midterm.r$below_cases[i] <- midterm.r$incr_cases[i]
+  }
+}
+View(midterm.r)
+
+
+for (i in 1:n) {
+  if(midterm.r$above_cases[i]==1) {
+    midterm.r$above_cases[i] <- NA
+  } else {}
+  if(midterm.r$below_cases[i]==1) {
+    midterm.r$below_cases[i] <- NA
+  } else {}
+}
+
+# Plotting
+
+p <- ggplot(data = midterm.r,
+            mapping = aes(x = Date,
+                          y = incr_cases))
+p = ggplot() + 
+  geom_point(data = midterm.r, aes(x = Date, y = above_cases), color = "blue") +
+  geom_point(data = midterm.r, aes(x = Date, y = below_cases), color = "red") +
+  labs(x = "03/19/2020-03/18/2021", y = "Change in Adjusted Closing Price",
+       title = "Changes in AAPL Daily Prices over Last Year",
+       subtitle = "Blue = Up, Red = Down, Alisha McDonnell",
+       caption = "Exam 1")
+
+p
+
+
+
+##############################
+
+
+
